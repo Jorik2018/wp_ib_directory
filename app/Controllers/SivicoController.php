f450ed7a032c0dbb8b204504bd27b5fb15cbf14d
Jorik2018 <antoniopinedo2018@gmail.com>
2025-06-12 23:01:11 -0500

    x

--- app/Controllers/SivicoController.php
+++ app/Controllers/SivicoController.php
@@ -43,7 +43,8 @@ class SivicoController extends Controller
         $to = $request['to'];
 
         $wpdb->last_error  = '';
-        $results = $wpdb->get_results("SELECT SQL_CALC_FOUND_ROWS * FROM grupoipe_regexa_ecr.ipress_red d ORDER BY red" . ($to ? "LIMIT " . $from . ', ' . $to : ""), ARRAY_A);
+        $db = get_option("db_master", "grupoipe_regexa_ecr");
+        $results = $wpdb->get_results("SELECT SQL_CALC_FOUND_ROWS * FROM $db.ipress_red d ORDER BY red" . ($to ? "LIMIT " . $from . ', ' . $to : ""), ARRAY_A);
         if ($wpdb->last_error) return new \WP_Error(500, $wpdb->last_error, array('status' => 500));
         foreach ($results as &$r) {
             foreach ($r as $key => &$value) {
