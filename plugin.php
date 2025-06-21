5d60078e508e0ad570babde79f1053e2918cf4c6
jorik2018 <antoniopinedo2018@gmail.com>
2025-06-21 10:49:32 -0500

    x

--- plugin.php
+++ plugin.php
@@ -309,7 +309,9 @@ function directory_install()
   $wpdb->query($sql);
 
 if ($wpdb->last_error) {
-  die($wpdb->last_error);
+  $log_file = __DIR__ . '/error_log_plugin.txt'; // Puedes cambiar la ruta
+  $mensaje = '[' . date('Y-m-d H:i:s') . '] ' . $wpdb->last_error . PHP_EOL;
+  file_put_contents($log_file, $mensaje, FILE_APPEND);
 }
   $sql = "USE $original_db_name";
   $wpdb->query($sql);
